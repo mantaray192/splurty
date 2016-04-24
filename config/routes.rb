9a859dbbc0c1b69984d6a0db5ed3d77b41ff4155
Daniel McCarvel <danpanel13@hotmail.com>
2016-04-24 20:57:26 +0000

    Added heading to website

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Splurty::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+root 'quotes#index'
+
   # Example resource route with options:
   #   resources :products do
   #     member do
